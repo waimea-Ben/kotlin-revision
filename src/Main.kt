90e94177bc682e18c9b705398b4dc2737a89f656
Master Benjamin <168691813+waimea-Ben@users.noreply.github.com>
2025-02-19 12:26:17 +1300

    sorted

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Hello World!")
+    showmsg("tree")
+}
+//ts fun takes one arg/parameter, and does not return any value unless called
+fun showmsg(text: String){
+    println(text)
+}
